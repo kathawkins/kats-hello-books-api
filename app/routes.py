a5c6a78a3be748ab9efc9b4d0d0cef357e90d003
Kat <kmoreno1205@gmail.com>
2022-10-19 15:07:50 -0600

    created Getting All Books Endpoint.

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-hello_world_bp = Blueprint("hello_world", __name__)
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    my_beautiful_response_body = "Hello, World!"
-    return my_beautiful_response_body
+books = [
+    Book(1, "Fictional Book TitleA", "A fantasy novel set in an old imaginary world."),
+    Book(2, "Fictional Book TitleB", "A fantasy novel set in a modern imaginary world."),
+    Book(3, "Fictional Book TitleC", "A fantasy novel set in a futuristic imaginary world.")
+] 
 
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    # return books_response
+    return jsonify(books_response)
