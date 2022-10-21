c6c8a458a840ed571efab20a99ee5a6b20acf479
Kat <kmoreno1205@gmail.com>
2022-10-21 13:00:01 -0600

    defined /books route to read one book endpoint

--- app/routes.py
+++ app/routes.py
@@ -25,3 +25,14 @@ def handle_books():
         })
     # return books_response
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
