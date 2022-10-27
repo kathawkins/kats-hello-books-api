0dda009d6fbec26c56ae3e16023cd98edabd4939
Kat <kmoreno1205@gmail.com>
2022-10-27 11:23:27 -0600

    implement a route that creates model records

--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,16 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-#hardcoded book data that was replaced with a model:
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
 
-# books = [
-#     Book(1, "Fictional Book TitleA", "A fantasy novel set in an old imaginary world."),
-#     Book(2, "Fictional Book TitleB", "A fantasy novel set in a modern imaginary world."),
-#     Book(3, "Fictional Book TitleC", "A fantasy novel set in a futuristic imaginary world.")
-# ] 
+    db.session.add(new_book)
+    db.session.commit()
 
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })
-#     return jsonify(books_response)
-
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
-
-#     return {
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description
-#     }
-
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
-
-#     for book in books:
-#         if book.id == book_id:
-#             return book
-
-#     abort(make_response({"message":f"book {book_id} not found"}, 404))
+    return make_response(f"Book {new_book.title} successfully created", 201)
