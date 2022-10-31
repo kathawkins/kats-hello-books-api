02df03ad7e37f99244d9477cf73e082b8606dd70
Kat <kmoreno1205@gmail.com>
2022-10-31 10:16:42 -0600

    add read one book endpoint

--- app/routes.py
+++ app/routes.py
@@ -26,3 +26,24 @@ def read_all_books():
             "description": book.description
         })
     return jsonify(books_response)
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+    return book
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id):
+    book = validate_book(book_id)
+    return {"id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
