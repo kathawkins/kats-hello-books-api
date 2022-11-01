3a050743c6c1a0b38cd4203c512e72a136c6cc69
Kat <kmoreno1205@gmail.com>
2022-11-01 13:57:26 -0600

    Add GET/book/<book_id> test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,15 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.book import Book
 
-
+#initialize test database
 @pytest.fixture
 def app():
     app = create_app({"TESTING": True})
 
-    #function below will be invoked after any request
-    #is completed
+    #function below this decorator will be invoked after
+    #any request is completed
     @request_finished.connect_via(app)
     def expire_session(sender, response, **extra):
         #creates a new database session so that we can
@@ -31,3 +32,17 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_books(app):
+    # Arrange
+    ocean_book = Book(title="Ocean Book",
+                    description="watr 4evr")
+    mountain_book = Book(title="Mountain Book",
+                        description="i luv 2 climb rocks")
+
+    db.session.add_all([ocean_book, mountain_book])
+    # Alternatively, we could do:
+    # db.session.add(ocean_book)
+    # db.session.add(mountain_book)
+    db.session.commit()
