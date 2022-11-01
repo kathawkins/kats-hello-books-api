e59bd5322a5c2f722f1b84e0937c5da5286c2f16
Kat <kmoreno1205@gmail.com>
2022-11-01 14:31:37 -0600

    Added tests for post and get/books

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,3 +19,44 @@ def test_get_one_book(client, two_saved_books):
         "title": "Ocean Book",
         "description": "watr 4evr"
     }
+
+def test_get_one_book_with_no_records(client):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+
+def test_create_one_book(client):
+    # Act
+    response = client.post("/books", json={
+        "title": "New Book",
+        "description": "The Best!"
+    })
+    response_body = response.get_json() #the get_json method 
+    # will fail if the POST/books route does not jsonify the
+    # response body; an alternative would be to use:
+    # response_body = response.get_data(as_text=True)
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == "Book New Book successfully created"
+
+def test_get_all_books(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [{
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    },
+    {
+        "id": 2,
+        "title": "Mountain Book",
+        "description": "i luv 2 climb rocks"
+    }]
